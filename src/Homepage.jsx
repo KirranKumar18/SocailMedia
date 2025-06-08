40b355e24008b6ae00951782915831ac69f4e011
KirranKumar18 <kirrankumar2005@gmail.com>
2025-06-08 23:14:05 +0530

    second day

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,27 +1,43 @@
 import './HomePage.css'
+import { supabase } from './Supabase.js'
 
 export default function HomePage(){
 
+const MsgSender = async(Sender,Message,msg_Time)=>{
+
+const { error } = await supabase
+                .from("Chat")
+                .insert([{Sender,Message,msg_Time}]);
+}
+if(error){
+    alert("there was an error "+error)
+}
+else{
+    alert("No error")
+}
+
+
     return(
         <>
-     
-
         <div className="parent">
 
             <div className="left"> 
 
                 <h1>left</h1>
+                <button className='Hp-btn'>New Chat</button>
             </div>
 
             <div className='main-holder'>
 
             <div className="top"> 
 
-                    <h1>top</h1>
+                    <h1>NIGGAS IN PRESIDENCY</h1>
             </div>
             <hr/>
+
             <div className="main"> 
                     <h1>body</h1>
+
             </div>
 
 
